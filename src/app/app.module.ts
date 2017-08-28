80dba2294dcc9fbf072fb1ec162c7fa16200bc30
Ricardo Bomfim <ricardoub@gmail.com>
2017-08-28 10:22:07 -0300

    aula44-tratamento-de-erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
-import { RestaurantsService } from './restaurants/restaurants.service'
+import { RestaurantsService } from './restaurants/restaurants.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { RestaurantsService } from './restaurants/restaurants.service'
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
   ],
   imports: [
     BrowserModule,
