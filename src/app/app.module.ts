6ee45703921a12df4333419169f143b6b9b61077
Ricardo Bomfim <ricardoub@gmail.com>
2017-08-29 14:32:18 -0300

    aula57-template-forms-compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms'
 import { ROUTES } from './app.routes'
 
 import { AppComponent } from './app.component';
@@ -38,7 +38,8 @@ import { OrderComponent } from './order/order.component';
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    FormsModule
   ],
   providers: [ RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'} ],
   bootstrap: [AppComponent]
