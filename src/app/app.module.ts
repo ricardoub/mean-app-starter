fc7f9b867d85f0fbec58678b81cecd56904a8dbe
Ricardo Bomfim <ricardoub@gmail.com>
2017-08-31 09:21:59 -0300

    aula64 - interface controlvalueaccess

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { ReviewsComponent } from './restaurants/restaurant-detail/reviews/review
 import { ShoppingCartService } from "app/restaurants/restaurant-detail/shopping-cart/shopping-cart.service";
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
+//import { OrderItemsComponent } from './order/order-items/order-items.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +38,8 @@ import { InputComponent } from './shared/input/input.component';
     ReviewsComponent,
     OrderComponent,
     InputComponent,
+    RadioComponent,
+    //OrderItemsComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from "app/shared/radio/radio.option.model";
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions: RadioOption[] = [
+    {value: 'MON', label: 'Dinheiro'},
+    {value: 'DEB', label: 'Cartão de Débito'},
+    {value: 'REF', label: 'Cartão Refeição'}
+  ]
 
   constructor() { }
 
