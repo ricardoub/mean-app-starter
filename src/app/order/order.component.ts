0a42d032386d190274498c5ab745a54611999bd3
Ricardo Bomfim <ricardoub@gmail.com>
2017-09-04 14:05:08 -0300

    aula73-validação com reactive forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -5,13 +5,17 @@ import { CartItem } from "app/restaurants/restaurant-detail/shopping-cart/cart-i
 import { Order, OrderItem } from './order.model'
 import { Router } from '@angular/router'
 
-import { FormGroup, FormBuilder} from '@angular/forms'
+import { FormGroup, FormBuilder,Validators } from '@angular/forms'
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern = /^[0-9]*$/
+
 
   orderForm: FormGroup
 
@@ -31,13 +35,18 @@ export class OrderComponent implements OnInit {
 
   ngOnInit() {
     this.orderForm = this.formBuilder.group({
-      name:  this.formBuilder.control(''),
-      email: this.formBuilder.control(''),
-      emailConfirmation: this.formBuilder.control(''),
-      address: this.formBuilder.control(''),
-      number: this.formBuilder.control(''),
+      name:  this.formBuilder.control('', 
+        [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', 
+        [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('',
+        [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('',
+        [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('',
+        [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
-      paymentOption: this.formBuilder.control('')
+      paymentOption: this.formBuilder.control('', [Validators.required])
     })
   }
 
