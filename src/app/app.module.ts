4d5847bf8cd5a7a16fe804988254e0f1bea846cc
Ricardo Bomfim <ricardoub@gmail.com>
2017-09-04 13:55:01 -0300

    aula72-refatorando para usar reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { ROUTES } from './app.routes'
 
 import { AppComponent } from './app.component';
@@ -26,6 +26,7 @@ import { OrderService } from "app/order/order.service";
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { RatingComponent } from './shared/rating/rating.component';
+
 
 @NgModule({
   declarations: [
@@ -52,7 +53,8 @@ import { RatingComponent } from './shared/rating/rating.component';
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ 
     RestaurantsService, 
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -5,11 +5,15 @@ import { CartItem } from "app/restaurants/restaurant-detail/shopping-cart/cart-i
 import { Order, OrderItem } from './order.model'
 import { Router } from '@angular/router'
 
+import { FormGroup, FormBuilder} from '@angular/forms'
+
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  orderForm: FormGroup
 
   delivery: number = 8
 
@@ -21,10 +25,20 @@ export class OrderComponent implements OnInit {
 
   constructor(
     private orderService: OrderService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name:  this.formBuilder.control(''),
+      email: this.formBuilder.control(''),
+      emailConfirmation: this.formBuilder.control(''),
+      address: this.formBuilder.control(''),
+      number: this.formBuilder.control(''),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('')
+    })
   }
 
   itemsValue(): number {
