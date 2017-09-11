3ab7f49cec227b3d3ea44622033238dcc4a666dc
Ricardo Bomfim <ricardoub@gmail.com>
2017-09-11 11:07:00 -0300

    aula103-animação barra pesquisa

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from "@angular/animations"
 
 import { Restaurant } from './restaurant/restaurant.model'
 import { RestaurantsService } from './restaurants.service'
@@ -6,9 +7,25 @@ import { RestaurantsService } from './restaurants.service'
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
+  styleUrls: ['./restaurants.component.css'],
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height": "0px"
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height": "70px",
+        "margin-top": "20px"
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
+
+  searchBarState = 'hidden'
 
   restaurants: Restaurant[]
 
@@ -17,6 +34,10 @@ export class RestaurantsComponent implements OnInit {
   ngOnInit() {
     this.restaurantsService.restaurants()
       .subscribe(restaurants => this.restaurants = restaurants)
+  }
+
+  toggleSearch(){
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
   }
 
 }
